33b1e55602323917059bccf1a6cad92cca173dcb
Esmail Shabayek <esmail.shabayek@gmail.com>
2025-12-13 15:34:52 +0200

    Change font to cairo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,8 +33,13 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+        />
+
         <script src="{{ asset('js/jquery.min.js') }}"></script>
         @routes
         @if (app()->environment('local'))
