71c4d73eead6ed04734fd7690b8a336d41f3b560
Esmail Shabayek <esmail.shabayek@gmail.com>
2026-01-16 23:15:39 +0200

    Add Clarity tracking script to app layout for enhanced user analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uzzmvffhqo");
+        </script>
+
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
             (function() {
