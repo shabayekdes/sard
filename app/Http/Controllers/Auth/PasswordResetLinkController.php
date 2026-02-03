cf18b0b5b0a8558e68d1a0f8d0dc9390b7b4cf16
Esmail Shabayek <esmail.shabayek@gmail.com>
2026-02-03 21:23:20 +0200

    remove debug

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -3,12 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use App\Notifications\CustomResetPassword;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
-use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -17,9 +14,8 @@ class PasswordResetLinkController extends Controller
     /**
      * Show the password reset link request page.
      */
-    public function create(Request $request)
+    public function create(Request $request): Response
     {
-        return (new CustomResetPassword(Str::random(10)))->toMail(User::first());
         return Inertia::render('auth/forgot-password', [
             'status' => $request->session()->get('status'),
             'settings' => settings(),
