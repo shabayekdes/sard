44d8e9c7cf8e4f89f224efca1c047b7d96d1076f
Esmail Shabayek <esmail.shabayek@gmail.com>
2026-03-31 21:38:01 +0200

    Add TenantRegisteredNotification for Slack notifications
    - Introduced a new notification class, TenantRegisteredNotification, to send alerts via Slack when a new tenant registers.
    - The notification includes company details such as ID, phone number, and email, formatted with Slack's Block Kit for improved presentation.
    - Updated composer.json to include the laravel/slack-notification-channel dependency, ensuring compatibility with the new notification feature.

---
+++ app/Notifications/Tenant/TenantRegisteredNotification.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Notifications\Tenant;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Slack\BlockKit\Blocks\ContextBlock;
+use Illuminate\Notifications\Slack\BlockKit\Blocks\SectionBlock;
+use Illuminate\Notifications\Slack\SlackMessage;
+
+class TenantRegisteredNotification extends Notification
+{
+    
+    public function __construct(protected $tenant)
+    {
+    }
+
+    public function via($notifiable)
+    {
+        return ['slack'];
+    }
+
+    public function toSlack($notifiable)
+    {
+        return (new SlackMessage)
+            ->text('A new company has been registered!')
+            ->username('Sard SAAS')
+            ->image('https://framerusercontent.com/images/8GV6LzJju4jTkXfQwE14n70BOg.png')
+            ->headerBlock(':tada: New Registration: ' . $this->tenant->name . ' :rocket:')
+            ->contextBlock(function (ContextBlock $block) {
+                $block->text('Company ID #' . $this->tenant->id);
+            })
+            ->sectionBlock(function (SectionBlock $block) {
+                $block->text('Company Details:');
+                $block->field("*Phone Number:*\n" . $this->tenant->mobile)->markdown();
+                $block->field("*Email:*\n" . $this->tenant->email)->markdown();
+            })
+            ->dividerBlock()
+            ->sectionBlock(function (SectionBlock $block) {
+                $block->text('Please activate company on https://' . config('app.domain'))->verbatim();
+            });
+    }
+}
